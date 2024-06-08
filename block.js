0635add901bb1860816a8f673c29441a9b424307
kenmori <kenjimoritata@gmail.com>
2024-06-09 08:08:40 +0900

    feat: block

---
+++ block.js
@@ -0,0 +1,11 @@
+class Block {
+  constructor(lastHash, timestamp, data, hash) {
+    this.lastHash = lastHash
+    this.timestamp = timestamp;
+    this.data = data;
+    this.hash = hash;
+  }
+}
+
+const block1 = new Block('foo-lastHash', "01/01/01", 'foo-data', 'foo-hash');
+
