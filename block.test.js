3f06969b01e4b8cdc6c3288a695a1ba17a319a37
kenmori <kenjimoritata@gmail.com>
2024-06-09 10:08:16 +0900

    feat: config for genesis data

--- block.test.js
+++ block.test.js
@@ -1,15 +1,29 @@
 const Block = require('./block');
+const { GENESIS_DATA, MINE_RATE } = require('./config');
 
 describe('Block', () => {
-  const hash = 'foo-hash';
-  const lastHash = 'foo-lastHash';
-  const timestamp = '01/01/01';
-  const data = 'foo-data';
-  it("has timestamp, lastHash, hash, data", () => {
-    const block = new Block(lastHash, timestamp, data, hash);
-    expect(block.hash).toEqual(hash);
-    expect(block.lastHash).toEqual(lastHash);
+  const timestamp = 2000;
+  const lastHash = 'foo-hash';
+  const hash = 'bar-hash';
+  const data = ['blockchain', 'data'];
+  const block = new Block({ timestamp, lastHash, hash, data });
+
+  it('has a timestamp, lastHash, hash, and data property', () => {
     expect(block.timestamp).toEqual(timestamp);
+    expect(block.lastHash).toEqual(lastHash);
+    expect(block.hash).toEqual(hash);
     expect(block.data).toEqual(data);
-  })
-})
+  });
+
+  describe('genesis()', () => {
+    const genesisBlock = Block.genesis();
+
+    it('returns a Block instance', () => {
+      expect(genesisBlock instanceof Block).toBe(true);
+    });
+
+    it('returns the genesis data', () => {
+      expect(genesisBlock).toEqual(GENESIS_DATA);
+    });
+  });
+});
