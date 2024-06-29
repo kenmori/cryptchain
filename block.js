3c8de6d758de88cbdf998164fbfd33806029e22c
kenmori <kenjimoritata@gmail.com>
2024-06-29 19:27:55 +0900

    wip

--- block.js
+++ block.js
@@ -3,11 +3,13 @@ const { GENESIS_DATA } = require('./config');
 const cryptoHash = require('./crypto-hash');
 
 class Block {
-  constructor({ timestamp, lastHash, hash, data }) {
+  constructor({ timestamp, lastHash, hash, data, nonce, difficulty }) {
     this.timestamp = timestamp;
     this.lastHash = lastHash;
     this.hash = hash;
     this.data = data;
+    this.nonce = nonce;
+    this.difficulty = difficulty;
   }
 
   static genesis() {
@@ -17,12 +19,17 @@ class Block {
   static mineBlock({ lastBlock, data }) {
     const timestamp = Date.now();
     const lastHash = lastBlock.hash;
+    const { difficulty } = lastBlock;
+    // マイニングアルゴリズム実行中にnonceを調整できるようにするため
+    let nonce = 0;
 
     return new this({
       timestamp: Date.now(),
       lastHash: lastBlock.hash,
       data,
-      hash: cryptoHash(timestamp, lastHash, data)
+      hash: `${"0".repeat(lastBlock.difficulty)}${cryptoHash(timestamp, lastHash, data)}`,
+      nonce: lastBlock.nonce,
+      difficulty: lastBlock.difficulty
     });
   }
 }
